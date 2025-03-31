0e861b3a5931005fd8a30eb5e76a09bb412fb1d0
lservo <lucas.servo@paipe.co>
2025-03-31 09:07:13 -0300

    feat: adding cognito auto settings

--- src/shared/infra/settings/aws-cognito/index.ts
+++ src/shared/infra/settings/aws-cognito/index.ts
@@ -1,58 +1,27 @@
-// import { container } from 'tsyringe';
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { container } from "tsyringe";
+import { CognitoProvider } from "../../containers/providers/cognito-provider/cognito-provider.js";
 
-// import mail from '@/shared/application/mail';
+export const setupCognitoUserPool = async () => {
+	const isLocal =
+		process.env.NODE_ENV === "local" || process.env.LOCAL === "true";
 
-// import { CognitoProvider } from '../../container/providers/CognitoProvider/implementations/CognitoProvider';
-// import { SESProvider } from '../../container/providers/SESProvider/implementations/SESProvider';
+	if (!isLocal) {
+		const cognitoProvider = container.resolve(CognitoProvider);
 
-// export const setupCognitoUserPool = async () => {
-//     const isLocal =
-//         process.env.NODE_ENV === 'local' || process.env.LOCAL === 'true';
+		const inviteTemplate = readFileSync(
+			resolve("src/shared/infra/settings/aws-cognito/invite.hbs"),
+			"utf-8",
+		);
+		const verifyTemplate = readFileSync(
+			resolve("src/shared/infra/settings/aws-cognito/recovery-password.hbs"),
+			"utf-8",
+		);
 
-//     if (!isLocal) {
-//         const sesProvider = container.resolve(SESProvider);
-//         const cognitoProvider = container.resolve(CognitoProvider);
-
-//         const inviteTemplate = await sesProvider.insertEmailVariables({
-//             path: 'src/shared/infra/settings/aws-cognito/invite.hbs',
-//             variables: {
-//                 siteUrl: mail.app_login_url,
-//                 googlePlayUrl:
-//                     'https://play.google.com/store/apps/details?id=br.com.gedott',
-//                 appStoreUrl:
-//                     'https://apps.apple.com/br/app/gedott/id1581060733',
-//                 googlePlayLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/google-play.png',
-//                 appStoreLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/app-store.png',
-//                 gedottLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/logo.png',
-//                 password: '{####}',
-//                 username: '{username}',
-//             },
-//         });
-
-//         const verifyTemplate = await sesProvider.insertEmailVariables({
-//             path: 'src/shared/infra/settings/aws-cognito/recovery-password.hbs',
-//             variables: {
-//                 siteUrl: mail.app_login_url,
-//                 googlePlayUrl:
-//                     'https://play.google.com/store/apps/details?id=br.com.gedott',
-//                 appStoreUrl:
-//                     'https://apps.apple.com/br/app/gedott/id1581060733',
-//                 googlePlayLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/google-play.png',
-//                 appStoreLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/app-store.png',
-//                 gedottLogo:
-//                     'https://sgdot-email-images.s3.amazonaws.com/logo.png',
-//                 tempPassword: '{####}',
-//             },
-//         });
-
-//         await cognitoProvider.setupPoolConfigs({
-//             inviteTemplate,
-//             verifyTemplate,
-//         });
-//     }
-// };
+		await cognitoProvider.setupPoolConfigs({
+			inviteTemplate,
+			verifyTemplate,
+		});
+	}
+};

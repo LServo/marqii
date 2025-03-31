5d992a82f68c710d296247f8089f12f6564f57de
lservo <lucas.servo@paipe.co>
2025-03-31 01:11:40 -0300

    feat: integrating cognito provider on user delete

--- src/modules/accounts/use-cases/user-delete/user-delete-use-case.ts
+++ src/modules/accounts/use-cases/user-delete/user-delete-use-case.ts
@@ -1,5 +1,6 @@
 import { notFound } from "@/shared/application/http-responses.js";
 import { SaveLogs } from "@/shared/application/save-logs.js";
+import type { ICognitoProvider } from "@/shared/infra/containers/providers/cognito-provider/cognito-provider.interface.js";
 import type { UUID } from "@/shared/main/@types/index.js";
 import { inject, injectable } from "tsyringe";
 import type { IUsersRepository } from "../../infra/repository/users-repository/users-repository.interface.js";
@@ -10,6 +11,8 @@ class UserDeleteUseCase {
 	constructor(
 		@inject("UsersRepository")
 		private usersRepository: IUsersRepository,
+		@inject("CognitoProvider")
+		private cognitoProvider: ICognitoProvider,
 	) {}
 
 	async execute({
@@ -22,6 +25,8 @@ class UserDeleteUseCase {
 		});
 
 		if (!userExists) throw notFound({ error_code: "USER_NOT_FOUND" });
+
+		await this.cognitoProvider.disableUser({ username: userExists.email });
 
 		await this.usersRepository.deleteUser({ id });
 	}

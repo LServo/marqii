d65e34b55b660599fc69945acac310bedfd0af54
lservo <lucas.servo@paipe.co>
2025-03-31 09:37:37 -0300

    refactor: adding auth validation on users routes

--- src/shared/main/routes/accounts/users.routes.ts
+++ src/shared/main/routes/accounts/users.routes.ts
@@ -5,30 +5,19 @@ import { UserDeleteController } from "@/modules/accounts/use-cases/user-delete/u
 import { UserReadController } from "@/modules/accounts/use-cases/user-read/user-read-controller.js";
 import { UserUpdateController } from "@/modules/accounts/use-cases/user-update/user-update-controller.js";
 import { expressRouter as execute } from "../../middlewares/index.js";
+import { validateAuth } from "../../middlewares/validate-auth.js";
 
 const v1UsersRoutes = Router();
 
-v1UsersRoutes.post(
-	"/create",
-	// validateAuth(ReadAccountRules),
-	execute(new UserCreateController()),
-);
+v1UsersRoutes.post("/create", execute(new UserCreateController()));
 
-v1UsersRoutes.put(
-	"/update",
-	// validateAuth(ReadAccountRules),
-	execute(new UserUpdateController()),
-);
+v1UsersRoutes.put("/update", validateAuth, execute(new UserUpdateController()));
 
-v1UsersRoutes.get(
-	"/read",
-	// validateAuth(ReadAccountRules),
-	execute(new UserReadController()),
-);
+v1UsersRoutes.get("/read", validateAuth, execute(new UserReadController()));
 
 v1UsersRoutes.delete(
 	"/delete",
-	// validateAuth(ReadAccountRules),
+	validateAuth,
 	execute(new UserDeleteController()),
 );
 
